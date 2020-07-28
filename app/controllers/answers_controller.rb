15e399812ac229f979b3d8557540710cea7e2a87
prosojchelovek <alaev.musost@mail.ru>
2020-07-28 21:29:15 +0300

    Added spec(create) for answer and a fix to trait

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,4 +1,24 @@
 class AnswersController < ApplicationController
   def new
   end
+
+  def create
+    @answer = question.answers.create(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def question
+    @question ||= Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
+  end
 end
