c4a1b5a0f816acbeef4107237f1e9921edc3b926
prosojchelovek <alaev.musost@mail.ru>
2020-07-28 20:40:03 +0300

    Added spec new for the answer

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe AnswersController, type: :controller do
+  let(:question) { create(:question) }
+
+  describe 'GET #new' do
+    before { get :new, params: { question_id: question } }
+
+    it 'render new view' do
+      expect(response).to render_template :new
+    end
+  end
+end
