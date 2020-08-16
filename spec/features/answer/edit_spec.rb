23112c26655ee50dbc706cca5daa244b44de62b9
prosojchelovek <alaev.musost@mail.ru>
2020-08-17 00:34:23 +0300

    The author can add a file when editing a answer

--- spec/features/answer/edit_spec.rb
+++ spec/features/answer/edit_spec.rb
@@ -46,10 +46,26 @@ feature 'User can edit his answer', %q{
       end
       expect(page).to have_content "Body can't be blank"
     end
+
     scenario "tries to edit other user's answer" do
       visit question_path(question)
 
       expect(page).to_not have_button 'Edit'
+    end
+
+    scenario "tries to add files", js: true do
+      sign_in user
+      visit question_path(question)
+      click_on 'Edit'
+
+      within '.answers' do
+        fill_in 'Your answer', with: 'another answer'
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+        click_on 'Save'
+      end
+      
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 end
